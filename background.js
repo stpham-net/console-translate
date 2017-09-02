b08ba523ee8a2c9ca59255e7524f4fa580bbffb9
Tung Pham <stpham621@gmail.com>
2017-09-03 06:46:36 +0700

    Reformat Code use WebStorm

--- background.js
+++ background.js
@@ -1,22 +1,22 @@
 // Khởi tạo storage
 if (localStorage.getItem("extensionStatus") === null) {
-	localStorage.setItem("extensionStatus", "true");
+  localStorage.setItem("extensionStatus", "true");
 }
 
 // Bật-Tắt tiện ích khi khởi động
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request === "getExtensionStatus") {
-      sendResponse(localStorage.getItem("extensionStatus"));
-    }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request === "getExtensionStatus") {
+    sendResponse(localStorage.getItem("extensionStatus"));
+  }
 });
 
 // Bật-Tắt tiện ích khi bấm vào icon
-chrome.browserAction.onClicked.addListener(function(tab) {
-	var extensionStatus = (localStorage.getItem("extensionStatus") == "true") ? "false" : "true";
-	chrome.tabs.sendMessage(tab.id, extensionStatus);
+chrome.browserAction.onClicked.addListener(function (tab) {
+  var extensionStatus = (localStorage.getItem("extensionStatus") === "true") ? "false" : "true";
+  chrome.tabs.sendMessage(tab.id, extensionStatus);
   localStorage.setItem("extensionStatus", extensionStatus);
-  chrome.browserAction.setIcon({path:"icon_" + extensionStatus + ".png"});
+  chrome.browserAction.setIcon({path : "icon_" + extensionStatus + ".png"});
 });
 
 // Tạo icon Bật-Tắt
-chrome.browserAction.setIcon({path:"icon_" + localStorage.getItem("extensionStatus") + ".png"});
+chrome.browserAction.setIcon({path : "icon_" + localStorage.getItem("extensionStatus") + ".png"});
--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 try {
   StphamTranslator = {
 
-    extensionStatus: null,
-    lastText: "",
+    extensionStatus : null,
+    lastText        : "",
 
     // Khởi tạo chương trình dịch
-    init() {
+    init () {
 
       // Bật-Tắt tiện ích khi khởi động
-      chrome.runtime.sendMessage("getExtensionStatus", function(response) {
+      chrome.runtime.sendMessage("getExtensionStatus", function (response) {
         StphamTranslator.extensionStatus = (response === "true");
       });
 
@@ -20,24 +20,24 @@ try {
       });
 
       // Lắng nghe hành động nhả chuột phải
-      document.addEventListener('mouseup', StphamTranslator.mouseup);
+      document.addEventListener("mouseup", StphamTranslator.mouseup);
 
     },
 
     // Lắng nghe hành động nhả chuột phải
-    mouseup(event) {
+    mouseup (event) {
       if (event.which !== 3) {
         StphamTranslator.run();
       }
     },
 
     // Chạy tiện ích
-    run() {
+    run () {
       if (StphamTranslator.extensionStatus === true) {
-        var selectionText = StphamTranslator.getSelectionText();
+        let selectionText = StphamTranslator.getSelectionText();
         if (selectionText !== "") {
-          var foreignText = selectionText.replace(/^\s*/, "").replace(/\s*$/, "");
-          if (StphamTranslator.lastText != foreignText) {
+          let foreignText = selectionText.replace(/^\s*/, "").replace(/\s*$/, "");
+          if (StphamTranslator.lastText !== foreignText) {
             StphamTranslator.lastText = foreignText;
             StphamTranslator.translate(foreignText);
           }
@@ -46,13 +46,13 @@ try {
     },
 
     // Thực hiện dịch
-    translate(foreignText) {
-        let url = "https://translate.googleapis.com/translate_a/single";
-        let param = "sl=auto&tl=vi&dt=t&q=" + encodeURIComponent(foreignText);
-        let urlFull = url + "?client=gtx&" + param;
-        // Dịch bằng API của Google Translate
+    translate (foreignText) {
+      let url = "https://translate.googleapis.com/translate_a/single";
+      let param = "sl=auto&tl=vi&dt=t&q=" + encodeURIComponent(foreignText);
+      let urlFull = url + "?client=gtx&" + param;
+      // Dịch bằng API của Google Translate
       StphamTranslator.loadAjax(urlFull).then(function (response) {
-        var responseText = "";
+        let responseText = "";
         for (i = 0; i < response[0].length; i++) {
           responseText += response[0][i][0];
         }
@@ -61,27 +61,29 @@ try {
       });
     },
 
-    complete(responseText) {
+    complete (responseText) {
       console.clear();
-      console.log("%c" + responseText, 'font-size: x-large'); // DEBUG
+      console.log("%c" + responseText, "font-size: x-large"); // DEBUG
     },
 
-
     // Lấy chữ của đoạn bôi đen
-    getSelectionText() {
-      var text = "";
-      var activeEl = document.activeElement;
-      var activeElTagName = document.activeElement.tagName.toLowerCase();
-      if ((activeElTagName == "textarea") || (activeElTagName == "input" && /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) && (typeof activeEl.selectionStart == "number")) {} else if (window.getSelection) {
-        text = window.getSelection().toString();
+    getSelectionText () {
+      let text = "";
+      let activeEl = document.activeElement;
+      let activeElTagName = document.activeElement.tagName.toLowerCase();
+      if ((activeElTagName === "textarea") || (activeElTagName === "input" && /^(?:text|search|password|tel|url)$/i.test(activeEl.type)) && (typeof activeEl.selectionStart === "number")) {
+      } else {
+        if (window.getSelection) {
+          text = window.getSelection().toString();
+        }
       }
       return text;
     },
 
     // Load Ajax
-    loadAjax(url) {
-      'use strict';
-      var xhr = new XMLHttpRequest();
+    loadAjax (url) {
+      "use strict";
+      let xhr = new XMLHttpRequest();
       return new Promise((resolve, reject) => {
         xhr.onreadystatechange = function () {
           if (xhr.readyState === 4) {
@@ -92,14 +94,14 @@ try {
             }
           }
         };
-        xhr.open('GET', url);
+        xhr.open("GET", url);
         xhr.send();
       });
     }
 
     // Cuối Object
-  }
-} catch(e) {
+  };
+} catch (e) {
   // Làm gì đó
 }
 
